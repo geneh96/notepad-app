e209e36a6800f9f683888962105399479c4ffbdb
geneh96 <geneh96@gmail.com>
2023-02-13 13:49:58 -0500

    use machine env var

--- notepad-app/Helper/DBHelper.cs
+++ notepad-app/Helper/DBHelper.cs
@@ -5,22 +5,25 @@ using System.Data;
 namespace notepad_app.Helper
 {
     public static class DBHelper
-    {   
-        public static string ReturnConnectionString()
+    {
+        public static string? ReturnConnectionString()
         {
-            string connectionString = string.Empty;
+            string? connectionString = string.Empty;
 
+            if (String.IsNullOrEmpty(Environment.GetEnvironmentVariable("NotepadConnectionString", EnvironmentVariableTarget.Machine)))
+            {
+                var _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                var config = _configuration.Build();
+                connectionString = config.GetConnectionString("NotepadConnectionString");
+            }
+            else
+                connectionString = Environment.GetEnvironmentVariable("NotepadConnectionString", EnvironmentVariableTarget.Machine);
 
-            var _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables();
-            var config = _configuration.Build();
-            connectionString = config.GetConnectionString("NotepadConnectionString");
-
-
-            return String.Format(connectionString);
+            return connectionString;
         }
         public static Tuple<DataTable, string> ConnectDB()
         {
-            string connectionString = DBHelper.ReturnConnectionString();
+            string? connectionString = DBHelper.ReturnConnectionString();
 
             string queryString = "SELECT * FROM notepad";
 
@@ -50,7 +53,7 @@ namespace notepad_app.Helper
 
         public static void CreateNewNote(string title, string notes)
         {
-            string connectionString = DBHelper.ReturnConnectionString();
+            string? connectionString = DBHelper.ReturnConnectionString();
             string queryString = $"INSERT INTO notepad VALUES ('{title}', '{notes}', '{DateTime.Now}');";
 
             DataTable notepadTable = new DataTable();
