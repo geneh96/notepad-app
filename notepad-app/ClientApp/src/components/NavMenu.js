35348a46ddd31ca4b5d8e24068be2347b6863027
geneh96 <geneh96@gmail.com>
2023-02-13 00:51:47 -0500

    remove unnecessary references

--- notepad-app/ClientApp/src/components/NavMenu.js
+++ notepad-app/ClientApp/src/components/NavMenu.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
-
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
   render() {
     return (
       <header>
